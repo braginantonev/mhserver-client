55651835590e1641c0132b24ec2d951e3bb04c5b
Bragin Anton <antonsayfermay@gmail.com>
2026-01-15 23:44:56 +0300

    [api/endpoints] set all constants to pub

--- api/src/endpoints.rs
+++ api/src/endpoints.rs
@@ -1,15 +1,15 @@
-// mhserver api version: 1.3.0
+// mhserver api version: 1.2.0
 
-const API_URL: &str = "/api/v1";
+pub const API_URL: &str = "/api/v1";
 
-mod auth {
-    const LOGIN: &str = "/users/login";
-    const REGISTRATION: &str = "/users/register";
+pub mod auth {
+    pub const LOGIN: &str = "/users/login";
+    pub const REGISTRATION: &str = "/users/register";
 }
 
-mod data {
-    const CREATE_CONNECTION: &str = "/files/connect";
-    const SAVE_FILE: &str = "/files/{uuid}/save";
-    const GET_FILE: &str = "/files/{uuid}/get";
-    const GET_SUM: &str = "/files/{uuid}/sum";
+pub mod data {
+    pub const CREATE_CONNECTION: &str = "/files/connect";
+    pub const SAVE_FILE: &str = "/files/{uuid}/save";
+    pub const GET_FILE: &str = "/files/{uuid}/get";
+    pub const GET_SUM: &str = "/files/{uuid}/sum";
 }
