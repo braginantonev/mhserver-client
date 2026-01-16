ede3683a516fcaa6a878779ea41043069ab72c9e
Bragin Anton <antonsayfermay@gmail.com>
2026-01-16 23:30:00 +0300

    [main/connect] create connect async func

---
+++ src/connect.rs
@@ -0,0 +1,13 @@
+use {
+    super::{ ApplicationState, ServerConnection },
+};
+
+pub async fn connect(conn: ServerConnection, state: ApplicationState) -> Result<ApplicationState, String> {
+    match api::ping::ping(conn.client, conn.addr.as_str()).await {
+        Ok(res) => {
+            if res { Ok(state.next()) }
+            else { Err("Wrong address or server is off".to_string()) }
+        },
+        Err(err) => { Err(format!("Connection error: {}", err.to_string())) }
+    }
+}
