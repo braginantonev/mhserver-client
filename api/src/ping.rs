e7702cd1d2c8d919481d0cd268782a40d831d085
Bragin Anton <antonsayfermay@gmail.com>
2026-01-16 17:57:41 +0300

    [api/ping] use reqwest to server ping endpoint instead ping

--- api/src/ping.rs
+++ api/src/ping.rs
@@ -1,6 +1,7 @@
-use std::net::ToSocketAddrs;
-
 use regex::Regex;
+use reqwest::StatusCode;
+
+use super::{endpoints, ServerError};
 
 fn addr_formatted(addr: &str) -> bool {
     let re = Regex::new(r"[a-z0-9:.]+[:][1-9][0-9]+").unwrap();
@@ -8,21 +9,21 @@ fn addr_formatted(addr: &str) -> bool {
 }
 
 // Return true if server available
-pub fn ping(addr: &str) -> Result<bool, super::ServerError> {
+pub async fn ping(http_client: reqwest::Client, addr: &str) -> Result<bool, ServerError> {
     if !addr_formatted(addr) {
         return Err(super::ServerError::new("address have bad syntax"))
     }
 
-    let ip = addr
-        .to_socket_addrs()
-        .unwrap()
-        .next()
-        .unwrap()
-        .ip();
+    let resp =  http_client.post(format!("https://{}{}{}", addr, endpoints::API_URL, endpoints::PING))
+        .send()
+        .await;
 
-    match ping::new(ip).send() {
-        Ok(_) => return Ok(true),
-        Err(_) => Ok(false),
+    match resp {
+        Ok(resp) => match resp.status() {
+            StatusCode::OK => Ok(true),
+            _ => Ok(false)
+        },
+        Err(err) => { Err(ServerError::new(err.to_string().as_str())) }
     }
 }
 
