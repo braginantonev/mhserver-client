6216f1e281ce04e2108b921474f62fbacf401565
Bragin Anton <antonsayfermay@gmail.com>
2026-01-22 20:05:34 +0300

    [api/auth] realize login and register endpoints

---
+++ api/src/authorization.rs
@@ -0,0 +1,53 @@
+use {
+    super::{
+        ServerError,
+        endpoints::{ 
+            build_url, API_V1, 
+            auth::{ LOGIN, REGISTRATION }
+        },
+    },
+    serde::{ Deserialize, Serialize },
+};
+
+
+// mhserver api version: 1.3.0
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    user: String,
+    pass: String,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str) -> Self {
+        Self { user: username.to_string(), pass: password.to_string() }
+    }
+}
+
+/// Return response body
+pub async fn login_v1(client: reqwest::Client, addr: &str, user: User) -> Result<String, ServerError> {
+    match client.get(build_url(addr, API_V1, LOGIN))
+        .json(&user)
+        .send()
+        .await {
+            Ok(resp) => {
+                if resp.status() != 200 { return Err(ServerError(resp.text().await.expect("failed get response text"))) }
+                else { return Ok(resp.text().await.expect("failed get response text")) }
+            }
+            Err(err) => { return Err(ServerError(err.to_string())) }
+        };
+}
+
+pub async fn register_v1(client: reqwest::Client, addr: &str, user: User) -> Result<(), ServerError> {
+    match client.post(build_url(addr, API_V1, REGISTRATION))
+        .json(&user)
+        .send()
+        .await {
+            Ok(resp) => {
+                if resp.status() != 200 { return Err(ServerError(resp.text().await.expect("failed get response text"))) }
+            }
+            Err(err) => { return Err(ServerError(err.to_string())) }
+        };
+    
+    Ok(())
+}
