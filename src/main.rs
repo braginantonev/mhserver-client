39a57b7c8f920cbe4f2af9fec021c2e6b289644e
Bragin Anton <antonsayfermay@gmail.com>
2026-01-29 20:13:48 +0300

    [main] add preparing callback realization

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,44 @@ mod http;
 
 use {
     api::authorization::User, 
+    config::Config, 
     std::{
         error::Error, 
         sync::Arc,
-    },
-    config::Config,
+    }, 
+    tokio::sync::Mutex,
 };
 
 slint::include_modules!();
 
-impl AppStates {
-    fn next(&self) -> AppStates {
-        match self {
-            AppStates::Connection => AppStates::Login,
-            AppStates::Login => todo!(),
-            AppStates::Register => todo!()
-        }
-    }
+enum UiActions {
+    ChangeAppState(AppStates),
+    ChangePreparingState(PreparingStates),
+    ShowNotification(String, NotificationType)
 }
 
-enum UiActions {
-    ChangeState(AppStates),
-    ShowNotification(String, NotificationType)
+impl UiActions {
+    fn run(self, win: MainWindow) {
+        match self {
+            UiActions::ChangeAppState(next) => win.set_state(next),
+            UiActions::ChangePreparingState(next) => win.set_prepare_state(next),
+            UiActions::ShowNotification(desc, r#type) => {
+                notification::show(win, desc.as_str(), r#type);
+            }
+        }
+    }
 }
 
 struct ConnectionInfo {
     client: reqwest::Client,
     server_address: String,
-    user_jwt: Option<String>,
 }
 
 impl ConnectionInfo {
-    fn new(client: reqwest::Client, srv_addr: &str, user_jwt: &str) -> Self { 
+    fn new(client: reqwest::Client, srv_addr: &str) -> Self { 
         Self { 
             client: client.clone(),
             server_address: srv_addr.to_string(), 
-            user_jwt: if user_jwt != "" { Some(user_jwt.to_string()) } else { None },
         }
     }
 }
@@ -56,62 +58,155 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .timeout(std::time::Duration::new(2, 0))
         .build()?;
 
-    let cfg = Arc::new(match Config::from_file() {
+    let cfg = Arc::new(Mutex::new(match Config::from_file() {
         Ok(cfg) => cfg,
-        Err(_) => {
-            main_window.set_state(AppStates::Connection); // Set start field
-            Config::default()
-        }
-    });
+        Err(_) => Config::default()
+    }));
 
-    let cb_win = win_weak.clone();
-    let cb_cfg = cfg.clone();
-    let cb_client = http_client.clone();
-    main_window.on_connect(move |server_addr| {
-        let win = cb_win.clone();
-        let http_client = cb_client.clone();
-        let current_state = win.upgrade().unwrap().get_state();
+    let (preparing_tx, preparing_rx) = tokio::sync::mpsc::channel::<String>(1);
+    let preparing_rx = Arc::new(Mutex::new(preparing_rx));
+    
+    // Setup server connection and jwt
+    main_window.on_start_preparing({
+        let win = win_weak.clone();
+        let cfg = cfg.clone();
+        let client = http_client.clone();
 
-        tokio::spawn(http::tools::ping(
-            win,
-            ConnectionInfo::new(http_client, cb_cfg.get_server_address().as_str(), cb_cfg.get_user_jwt().as_str()),
-            current_state
-        ));
-    });
+        move || {
+            let win = win.clone();
+            let cfg = cfg.clone();
+            let client = client.clone(); 
+            let rx = preparing_rx.clone();
 
-    let call_win = win_weak.clone();
-    let call_cfg = cfg.clone();
-    let cb_client = http_client.clone();
-    main_window.on_login(move |username, password| {
-        let win = call_win.clone();
-        let http_client = cb_client.clone();
+            tokio::spawn(async move {
+                let (server_address, jwt) = {
+                    let guard = cfg.lock().await;
+                    (guard.get_server_address(), guard.get_user_jwt()) 
+                };
 
-        tokio::spawn(http::authorization::login(
-            win,
-            ConnectionInfo::new(http_client, call_cfg.get_server_address().as_str(), call_cfg.get_user_jwt().as_str()), 
-            User::new(username.as_str(), password.as_str())
-        ));
-    });
+                println!("ping server with address = {}", server_address);
 
-    let call_win = win_weak.clone();
-    let call_cfg = cfg.clone();
-    let cb_client = http_client.clone();
-    main_window.on_register(move |username, password, verify| {
-        let win = call_win.clone();
-        let http_client = cb_client.clone();
+                // Ping server
+                match api::ping::ping(client.clone(), server_address.as_str()).await {
+                    Err(_) => {
+                        let _ = win.upgrade_in_event_loop(move |win| {
+                            win.set_prepare_state(PreparingStates::Connection);
+                        });
 
-        if password != verify {
-            let _ = win.upgrade_in_event_loop(move |win| {
-                notification::show(win, "Password and verify password not ident!", NotificationType::Error);
+                        // Wait jwt from login callback
+                        let server_address = rx.lock().await.recv().await.expect("failed receive address from chanel");
+                        cfg.lock().await.set_address(server_address.as_str());
+                    },
+                    _ => {}
+                };
+
+                if jwt == "" {
+                    let _ = win.upgrade_in_event_loop(|win| {
+                        win.set_prepare_state(PreparingStates::Login);
+                    });
+                }
+
+                // Wait jwt from login callback
+                let jwt = rx.lock().await.recv().await.expect("failed receive jwt from channel");
+                {
+                    let mut guard = cfg.lock().await;
+                    guard.set_user_jwt(jwt.as_str());
+                    guard.save_to_file();
+                }
+
+                let _ = win.upgrade_in_event_loop(|_win| {
+                    println!("end of preparing");
+                    //win.set_state(AppStates::Main);
+                });
+
+                rx.lock().await.close();
             });
-            return
         }
+    });
 
-        tokio::spawn(http::authorization::register(
-            win,
-            ConnectionInfo::new(http_client, call_cfg.get_server_address().as_str(), call_cfg.get_user_jwt().as_str()),
-            User::new(username.as_str(), password.as_str())
-        ));
+    // Used to send new server address to prepare callback
+    main_window.on_connect({
+        let win = win_weak.clone();
+        let client = http_client.clone();
+        let tx = preparing_tx.clone(); // Sender for preparing callback
+
+        move |server_addr| {
+            let win = win.clone();
+            let client = client.clone();
+            let tx = tx.clone();
+
+            tokio::spawn(async move {
+                if let Some(act) = match api::ping::ping(client, server_addr.as_str()).await {
+                    Ok(_) => {
+                        match tx.send(server_addr.to_string()).await {
+                            Err(err) => Some(UiActions::ShowNotification(err.0, NotificationType::Error)),
+                            _ => None
+                        }
+                    },
+                    Err(err) => Some(UiActions::ShowNotification(err.to_string(), NotificationType::Error))
+                } {
+                    let _ = win.upgrade_in_event_loop(|win| {
+                        act.run(win);
+                    });
+                };
+            });
+        }
+    });
+    
+    // Used to send new jwt to preparing callback
+    main_window.on_login({
+        let win = win_weak.clone();
+        let cfg = cfg.clone();
+        let client = http_client.clone();
+        let tx = preparing_tx.clone();
+
+        move |username, password| {
+            let win = win.clone();
+            let cfg = cfg.clone();
+            let http_client = client.clone();
+            let tx = tx.clone();
+
+            tokio::spawn(async move {
+                let server_address = cfg.lock().await.get_server_address();
+
+                if let Some(jwt) = http::authorization::login(
+                    win,
+                    ConnectionInfo::new(http_client, server_address.as_str()), 
+                    User::new(username.as_str(), password.as_str())
+                ).await {
+                    tx.send(jwt).await.expect("failed to send jwt to channel");
+                }
+            });
+        }
+    });
+    
+    main_window.on_register({
+        let win = win_weak.clone();
+        let cfg = cfg.clone();
+        let client = http_client.clone();
+
+        move |username, password, verify| {
+            let win = win.clone();
+            let cfg = cfg.clone();
+            let http_client = client.clone();
+
+            if password != verify {
+                let _ = win.upgrade_in_event_loop(move |win| {
+                    notification::show(win, "Password and verify password not ident!", NotificationType::Error);
+                });
+                return
+            }
+
+            tokio::spawn(async move {
+                let server_address = cfg.lock().await.get_server_address();
+
+                http::authorization::register(
+                    win,
+                    ConnectionInfo::new(http_client, server_address.as_str()),
+                    User::new(username.as_str(), password.as_str())
+                )
+            });
+        }
     });
 
     main_window.run()?;
