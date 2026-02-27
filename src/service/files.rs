8f2caf989dffbb0df25823245b658f4db305cfbd
Bragin Anton <antonsayfermay@gmail.com>
2026-02-27 15:07:15 +0300

    [service/files] add cd and get_files methods

--- src/service/files.rs
+++ src/service/files.rs
@@ -1,11 +1,11 @@
 use {
-    reqwest::Client,
-    crate::config::files::FileServiceConfig
+    crate::{NotificationType, actions::UiActions, config::files::FileServiceConfig}, api::data, reqwest::Client, std::{path::{Path, PathBuf}, str::FromStr}
 };
 
 pub struct FileManager {
     client: Client,
-    cfg: FileServiceConfig
+    cfg: FileServiceConfig,
+    active_dir: PathBuf
 }
 
 impl FileManager {
@@ -18,7 +18,11 @@ impl FileManager {
             Some(x) => x,
             None => FileServiceConfig::default(),
         };
-        Self { client: client.clone(), cfg }
+        Self { 
+            client: client.clone(),
+            cfg,
+            active_dir: PathBuf::from_str("/").expect("failed create default file manager path")
+        }
     }
 
     pub fn config(&self) -> &FileServiceConfig {
@@ -27,6 +31,37 @@ impl FileManager {
 
     pub fn config_mut(&mut self) -> &mut FileServiceConfig {
         &mut self.cfg
+    }
+
+    async fn get_files_from(&self, target_dir: &str) -> Result<Vec<data::FileInfo>, UiActions> {
+        match data::get_files_v1(
+            api::AuthorizedRequest::new(self.cloned_client(), self.cfg.srv_com().server_address(), self.cfg.srv_com().user_jwt()),
+            target_dir
+        ).await {
+            Ok(res ) => Ok(res),
+            Err(err) => Err(UiActions::ShowNotification(err.to_string(), NotificationType::Error))
+        }
+    }
+
+    /// Return vector of file infos from active dir
+    pub async fn get_files(&self) -> Result<Vec<data::FileInfo>, UiActions> {
+        self.get_files_from(self.active_dir.to_str().unwrap()).await
+    }
+
+    /// Change file manager active direction and return file infos
+    pub async fn change_dir(&mut self, target_dir: PathBuf) -> Result<Vec<data::FileInfo>, UiActions> {
+        let files = self.get_files_from(target_dir.to_str().unwrap()).await;
+
+        if files.is_ok() {
+            self.active_dir = target_dir;
+        }
+
+        files
+    }
+
+    /// This is similar change_dir(), which use a current active dir as root
+    pub async fn change_dir_from_current(&mut self, next_dir: &Path) -> Result<Vec<data::FileInfo>, UiActions> {
+        self.change_dir(self.active_dir.join(next_dir)).await
     }
 }
 
