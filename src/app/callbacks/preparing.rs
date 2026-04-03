40ae34dbaf293a43d1ed296ae530978ee08258df
Bragin Anton <antonsayfermay@gmail.com>
2026-04-03 11:13:59 +0300

    [callbacks] delete prepering config also add using tools service

--- src/app/callbacks/preparing.rs
+++ src/app/callbacks/preparing.rs
@@ -1,6 +1,6 @@
 use {
     crate::{
-        NotificationType, PreparingStates, actions::UiActions, app::{Application, ApplicationConfig}
+        NotificationType, PreparingStates, actions::UiActions, app::Application
     },
     slint::ComponentHandle, 
     std::sync::Arc,
@@ -9,20 +9,18 @@ use {
 
 
 impl Application {
-    pub fn init_preparing_callbacks(&self, preparing_cfg: Arc<RwLock<ApplicationConfig>>) {
+    pub fn init_preparing_callbacks(&self, tools_service: Arc<RwLock<crate::service::tools::ServerTools>>) {
         let win_weak = self.ui_window.as_weak();
 
         self.ui_window.on_change_preparing_state({
             let win = win_weak.clone();
-            let main_cfg = self.cfg.clone();
-            let pre_cfg = preparing_cfg.clone();
-            let client = self.http_client.clone();
+            let cfg = self.cfg.clone();
+            let tools = tools_service.clone();
 
             move |new_preparing_state| {
                 let win = win.clone();
-                let main_cfg = main_cfg.clone();
-                let pre_cfg = pre_cfg.clone();
-                let client = client.clone();
+                let cfg = cfg.clone();
+                let tools = tools.clone();
 
                 tokio::spawn(async move {
                     println!("go to preparing {:?}", new_preparing_state);
@@ -31,24 +29,25 @@ impl Application {
                             UiActions::ChangePreparingState(new_preparing_state.next())
                         },
                         PreparingStates::CheckConn => {
-                            UiActions::ChangePreparingState(match api::ping::ping(client, pre_cfg.read().await.server_com_config().server_address()).await {
-                                Ok(_) => new_preparing_state.next(),
-                                Err(_) => PreparingStates::Connection
+                            let api_cfg = cfg.read().await;
+
+                            UiActions::ChangePreparingState(if tools.read().await.ping(Some(api_cfg.server_api_config().base_path())).await {
+                                new_preparing_state.next()
+                            } else {
+                                PreparingStates::Connection
                             })
                         },
                         PreparingStates::CheckAuth => {
-                            UiActions::ChangePreparingState(if pre_cfg.read().await.server_com_config().user_jwt() == "" {
+                            UiActions::ChangePreparingState(if cfg.read().await.server_api_config().jwt() == "" {
                                 PreparingStates::Login
                             } else {
                                 new_preparing_state.next()
                             })
-                        }
+                        },
                         PreparingStates::End => {
-                            let prepared_cfg = pre_cfg.read().await.clone();
+                            cfg.read().await.save_to_file();
 
-                            main_cfg.write().await.update_from_self(prepared_cfg);
-                            main_cfg.read().await.save_to_file();
-
+                            //Todo: add multiply events update
                             let _ = win.upgrade_in_event_loop(|win| {
                                 win.invoke_update_services();
                             });
@@ -65,21 +64,26 @@ impl Application {
 
         self.ui_window.on_connect({
             let win = win_weak.clone();
-            let client = self.http_client.clone();
-            let pre_cfg = preparing_cfg.clone();
+            let cfg = self.cfg.clone();
+            let tools = tools_service.clone();
 
             move |srv_addr| {
                 let win = win.clone();
-                let client = client.clone();
-                let pre_cfg = pre_cfg.clone();
+                let cfg = cfg.clone();
+                let tools = tools.clone();
 
                 tokio::spawn(async move {
-                    match api::ping::ping(client, srv_addr.as_str()).await {
-                        Ok(_) => {
-                            pre_cfg.write().await.server_com_config_mut().set_server_address(srv_addr.as_str());
-                            UiActions::ChangePreparingState(PreparingStates::Connection.next())
-                        },
-                        Err(desc) => UiActions::ShowNotification(desc.to_string(), NotificationType::Error)
+                    if tools.read().await.ping(Some(srv_addr.as_str())).await {
+                        cfg.write().await.server_api_config_mut().set_base_path(srv_addr.as_str());
+
+                        //Todo: add multiply events update
+                        let _ = win.upgrade_in_event_loop(|win| {
+                            win.invoke_update_services();
+                        });
+                        
+                        UiActions::ChangePreparingState(PreparingStates::Connection.next())
+                    } else {
+                        UiActions::ShowNotification("Server is off or unavailable".to_owned(), NotificationType::Error)
                     }.run_in_event_loop(win);
                 });
             }
