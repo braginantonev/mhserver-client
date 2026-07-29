9cd6ebf9e981d3d2a5c648ec419fde64a626e0e1
Bragin Anton <antonsayfermay@gmail.com>
2026-07-29 13:38:33 +0300

    add version files to crates

--- api/gen/build.rs
+++ api/gen/build.rs
@@ -1,7 +1,6 @@
 use std::io::Write;
 
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
     let mut ver_file = std::fs::File::create_new("VERSION").expect("failed create version file");
     ver_file.write_all(env!("CARGO_PKG_VERSION").as_bytes()).expect("failed save application crate version");
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ mod repository;
 
 slint::include_modules!();
 
+pub const APPLICATION_VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[tokio::main]
 async fn main() -> Result<(), app::errors::ApplicationError> {
+    println!("mhserver client {APPLICATION_VERSION}");
+    println!("target api version: {}", api::API_VERSION);
+    
     let app = app::Application::new()?;
     
     app.run().await?;
