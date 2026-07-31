79326c9270cdc8b18de881a6672632e1b30282e6
Bragin Anton <antonsayfermay@gmail.com>
2026-07-31 19:09:08 +0300

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,17 +5,20 @@ if [[ !(-e Cargo.toml) ]]; then
     cd ..
 fi
 
-if [[ !(-e build/bin) ]]; then
-    mkdir build/bin/
-else
-    rm -rf build/bin/
+if [[ -e build/bin ]]; then
+    rm -rf build/bin
 fi
 
+mkdir build/bin/
+
 # linux build
+echo "start linux build"
 cargo build --release
-cp target/release/mhserver-client build/bin/mhserver-client
+cp target/release/mhserver-client build/bin
+
+echo
 
 # windows build
+echo "start windows build"
 cargo build --target x86_64-pc-windows-gnu --release
-cp target/x86_64-pc-windows-gnu/release/mhserver-client.exe build/bin/mhserver-client.exe
-
+cp target/x86_64-pc-windows-gnu/release/mhserver-client.exe build/bin
