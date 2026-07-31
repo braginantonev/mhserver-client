165279b8b5bd1eb50dab5f132fc0d8cf30c62abe
Bragin Anton <antonsayfermay@gmail.com>
2026-07-31 13:10:54 +0300

    [api] remove version file creation

--- api/gen/build.rs
+++
@@ -1,6 +0,0 @@
-use std::io::Write;
-
-fn main() {
-    let mut ver_file = std::fs::File::create("VERSION").expect("failed create version file");
-    ver_file.write_all(env!("CARGO_PKG_VERSION").as_bytes()).expect("failed save application crate version");
-}
