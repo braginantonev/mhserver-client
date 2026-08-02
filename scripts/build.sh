1a1f595ced263a6863de028ad76477a866bd3a23
Bragin Anton <antonsayfermay@gmail.com>
2026-08-02 21:51:33 +0300

    create tag after build

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,21 +5,29 @@ if [[ !(-e Cargo.toml) ]]; then
     cd ..
 fi
 
-tag_name=""
-echo -n "Tag name: "
-read tag_name
-
-git tag -a $tag_name -m "release $tag_name"
-git push --tags
-
 mkdir build/bin/
 
 # linux build
 echo "start linux build"
 cargo build --release
 
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 echo
 
 # windows build
 echo "start windows build"
 cargo build --target x86_64-pc-windows-gnu --release
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+tag_name=""
+echo -n "Tag name: "
+read tag_name
+
+git tag -a $tag_name -m "release $tag_name"
+git push --tags
