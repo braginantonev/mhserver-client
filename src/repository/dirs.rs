f121326465e80abe11ce23863c0d8abe0f6bd0e0
Bragin Anton <antonsayfermay@gmail.com>
2026-08-03 13:18:10 +0300

    fix windows default download dir

--- src/repository/dirs.rs
+++ src/repository/dirs.rs
@@ -1,3 +1,7 @@
 pub fn default_download_dir() -> std::path::PathBuf {
-    std::env::home_dir().unwrap().join("Downloads/mhserver-client")
+    #[cfg(target_os = "linux")]
+    return std::env::home_dir().unwrap().join("Downloads/mhserver-client");
+
+    #[cfg(target_os = "windows")]
+    return std::env::home_dir().unwrap().join("Downloads\\mhserver-client");
 }
