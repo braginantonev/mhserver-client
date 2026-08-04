6424754de766311aa846c561b5de5bbd857bfa1c
Bragin Anton <antonsayfermay@gmail.com>
2026-08-04 14:08:32 +0300

    use winit with opengl

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,12 @@ pub const APPLICATION_VERSION: &str = env!("CARGO_PKG_VERSION");
 async fn main() -> Result<(), app::errors::ApplicationError> {
     println!("mhserver client {APPLICATION_VERSION}");
     println!("target api version: {}", api::API_VERSION);
+
+    slint::BackendSelector::new()
+        .backend_name("winit".to_owned())
+        .require_opengl()
+        .select()
+        .expect("Unable to create Slint backend with OpenGL ES renderer");
     
     let app = app::Application::new()?;
     
